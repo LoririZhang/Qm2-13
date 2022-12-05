ad6efa94d37fde8eb60c1236a75ba946c8a4ea19
LoririZhang <114959919+LoririZhang@users.noreply.github.com>
2022-12-05 16:20:20 +0000

    Update Opinion_extract.py

--- Opinion_extract.py
+++ Opinion_extract.py
@@ -14,5 +14,22 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 pylab.rcParams['figure.figsize'] = (10., 8.)
 
-data = pandas.read_csv('google_data.csv')
-data.head()
+opinion = pandas.read_csv('opinion_data.csv')
+opinion.head()
+
+#?Create relative row and column for each states, including happening, worried, futuregen, harmUS, personal
+opinion1 = pd.DataFrame(np.random.randn(1,5))
+opinion1 = pd.DataFrame()
+opinion1.columns = ['happening','worried','futuregen','harmUS','personal']
+opinion1.index = ['Alabama', 'Alaska', 'Arizona','Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia',
+                  'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 
+                  'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+                  'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
+                  'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+opinion1
+
+#? Create a new file contain the average of opinion index
+
+
+
+
